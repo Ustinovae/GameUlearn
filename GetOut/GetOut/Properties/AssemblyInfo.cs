f8f6d9822894bb76bd6678618e93481230bcf43c
Ustinovae <ustinov.alescha2015@yandex.ru>
2021-04-13 20:37:35 +0500

    Надеюсь

--- GetOut/GetOut/Properties/AssemblyInfo.cs
+++ GetOut/GetOut/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8fdbd3a0-9daa-4973-a06d-7d8bbddde37b")]
+[assembly: Guid("315ed2d4-aee7-48a5-a4e0-5f2c2d720fef")]
 
 // Version information for an assembly consists of the following four values:
 //
