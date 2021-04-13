939cb8323fdd21b2d4eac6f59cdebfbd68853a33
Ustinovae <ustinov.alescha2015@yandex.ru>
2021-04-13 18:03:39 +0500

    Added models

---
+++ GetOut/Models/Barrier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetOut.Models
+{
+    class Barrier
+    {
+        private Point location;
+
+        public Barrier(int x, int y)
+        {
+            location.X = x;
+            location.Y = y;
+        }
+
+        public Point Location
+        {
+            get
+            {
+                return location;
+            }
+        }
+    }
+}
---
+++ GetOut/Models/Furniture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetOut.Models
+{
+    class Furniture
+    {
+        private Point location;
+        private readonly int sizeStep; // надо будет сделать глобальную перемненну. чтобы можно было сылться из сущности на неё
+
+        public Furniture(int x, int y, int sizeStep)
+        {
+            location.X = x;
+            location.Y = y;
+            this.sizeStep = sizeStep;
+        }
+
+
+        public void MoveTo(Point direction)
+        {
+            location = new Point(direction.X * sizeStep, direction.Y * sizeStep);
+        }
+
+        public Point Location
+        {
+            get
+            {
+                return location;
+            }
+        }
+    }
+}
